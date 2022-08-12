84dd43d3382d006d9a8a1708755a1b8e768fefe7
Samandar <samandarnfh.2020@gmail.com>
2022-08-12 11:58:31 +0500

    17

--- logic17.py
+++ logic17.py
@@ -6,4 +6,10 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    x1=a%10
+    x2=a//10%10
+    x3=a//100%10 
+    x4=a//1000%10
+    x5=a//10000%10
+    return x1 < x2 < x3 < x4 < x5
+print(main(75421))
